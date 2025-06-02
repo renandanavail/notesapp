bcf6e3f4e557226c1f37ae3f4150b49c8d24c0e6
renandanavail <renandanavail2@gmail.com>
2025-06-02 10:48:36 +0700

    add new note

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_notes_app/models/note.dart';
+import 'package:flutter_notes_app/services/note_database.dart';
 import 'package:flutter/material.dart';
 
 class NotePage extends StatefulWidget {
@@ -8,8 +10,64 @@ class NotePage extends StatefulWidget {
 }
 
 class _NotePageState extends State<NotePage> {
+  // notes db
+  final notesDatabase = NoteDatabase();
+
+  // text controller
+  final noteController = TextEditingController();
+
+  // user wants to add new note
+  void addNewNote() {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: const Text("New Note"),
+            content: TextField(controller: noteController),
+            actions: [
+              // cancel button
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  noteController.clear();
+                },
+                child: const Text("Cancel"),
+              ),
+
+              // save button
+              TextButton(
+                onPressed: () {
+                  // create a new note
+                  final newNote = Note(content: noteController.text);
+                  // save in db
+                  notesDatabase.createNote(newNote);
+
+                  Navigator.pop(context);
+                  noteController.clear();
+                },
+                child: const Text("Save"),
+              ),
+            ],
+          ),
+    );
+  }
+
+  // user wants to update note
+
+  // user wants to delete note
+
+  // BUILD UI
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Notes")));
+    return Scaffold(
+      // App Bar
+      appBar: AppBar(title: const Text("Notes")),
+
+      // Button
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewNote,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
